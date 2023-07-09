7baae888b9abf2de3b22e9eb507d1c7f28f22ca4
JL710 <76447362+JL710@users.noreply.github.com>
2023-07-09 22:10:12 +0200

    edit main.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use workflowo::cli;
+use workflowo::yaml_parser;
 
 fn main() {
     let args = cli::parse_and_validate_args();
-    println!("{:?}", args);
+
+    let jobs = yaml_parser::jobs_from_file(args.file);
+    for job in jobs {
+        println!("{}", job);
+    }
 }
