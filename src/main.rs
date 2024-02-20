bfb7a84b55868ba133c9213eb6cf96911e1df5af
JL710 <76447362+JL710@users.noreply.github.com>
2024-02-20 14:39:50 +0100

    Refactor error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,30 @@ use workflowo::cli;
 use workflowo::tasks::Task;
 use workflowo::yaml_parser;
 
+pub fn error_chain_string(error: anyhow::Error) -> String {
+    let mut message = String::new();
+
+    let mut chain_iter = error.chain();
+
+    message += &format!("Error: {}\n", chain_iter.next().unwrap());
+
+    for err in chain_iter {
+        message += &format!("\nCaused by:\n\t{}", err);
+    }
+
+    message
+}
+
 fn main() {
     let args = cli::parse_and_validate_args();
 
-    let jobs = yaml_parser::jobs_from_file(args.file);
+    let jobs = match yaml_parser::jobs_from_file(args.file) {
+        Ok(x) => x,
+        Err(err) => {
+            println!("{}", error_chain_string(err));
+            std::process::exit(1);
+        }
+    };
     for job in &jobs {
         if args.verbose {
             println!("{}", job);
@@ -13,7 +33,8 @@ fn main() {
         if job.name == args.job {
             println!("Executing Job {}", job.name);
             if let Err(error) = job.execute() {
-                eprintln!("Job failed:\n╔══\n{}", error);
+                println!("{}", error_chain_string(error));
+                std::process::exit(1);
             }
             return;
         }
